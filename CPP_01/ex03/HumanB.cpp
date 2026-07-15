e2a7ff040a9c310c78e5985ef49a6baa483ab8ff
Pauline Swirgiel <pswirgie@1E2.42angouleme.fr>
2026-07-15 14:09:21 +0200

    feat(ex03): different verisons

--- CPP_01/ex03/HumanB.cpp
+++ CPP_01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: pswirgie <pswirgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 13:16:35 by pswirgie          #+#    #+#             */
-/*   Updated: 2026/07/15 14:07:28 by pswirgie         ###   ########.fr       */
+/*   Updated: 2026/07/15 14:08:59 by pswirgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 HumanB::HumanB ( std::string humanName )
 	: name(humanName)
-{}
+{
+	weapon = NULL;
+}
 
 void	HumanB::attack( void )
 {
-	std::cout << name << " attacks with their " << weapon->getType();
+	std::cout << name << " attacks with their " << (*weapon).getType();
 	std::cout << std::endl;
 }
 
